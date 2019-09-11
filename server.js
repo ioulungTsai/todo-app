f62a08f7e194337cc7e51747c1800dd2888f5c37
brandonTsai <brandonya125@gmail.com>
2019-09-11 14:40:08 -0700

    Reading data from mongoDB

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let app = express()
 let db
 
 let connectString = 'mongodb+srv://todoAppUser:brandon9456@cluster0-chl2r.mongodb.net/TodoApp?retryWrites=true&w=majority'
-mongodb.connect(connectString, {useNewUrlParser: true}, function(err, client) {
+mongodb.connect(connectString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   db = client.db()
   app.listen(3001)
 })
@@ -13,59 +13,51 @@ mongodb.connect(connectString, {useNewUrlParser: true}, function(err, client) {
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do App</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="display-4 text-center py-1">To-Do App</h1>
+  db.collection('items').find().toArray(function(err, items) {
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple To-Do App</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+      </head>
+      <body>
+        <div class="container">
+          <h1 class="display-4 text-center py-1">To-Do App</h1>
 
-      <div class="jumbotron p-3 shadow-sm">
-        <form action="/create-item" method="POST">
-          <div class="d-flex align-items-center">
-            <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-            <button class="btn btn-primary">Add New Item</button>
+          <div class="jumbotron p-3 shadow-sm">
+            <form action="/create-item" method="POST">
+              <div class="d-flex align-items-center">
+                <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                <button class="btn btn-primary">Add New Item</button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
 
-      <ul class="list-group pb-5">
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #1</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #2</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #3</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-      </ul>
+          <ul class="list-group pb-5">
+            ${items.map(function(item) {
+              return `
+                <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+                  <span class="item-text">${item.text}</span>
+                  <div>
+                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                  </div>
+                </li>`
+            }).join('')}
+          </ul>
 
-    </div>
+        </div>
 
-  </body>
-  </html>`)
+      </body>
+      </html>`)
+  })
 })
 
 app.post('/create-item', function(req, res) {
   db.collection('items').insertOne({text: req.body.item}, function() {
-    res.send('Thanks for submitting the form.')
+    res.redirect('/')
   })
 })
