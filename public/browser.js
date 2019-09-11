3622c28c3287adbf3518c9f865839190da36176f
brandonTsai <brandonya125@gmail.com>
2019-09-11 16:06:42 -0700

    Update edited todo item both in client side and mongoDB

--- public/browser.js
+++ public/browser.js
@@ -1,10 +1,12 @@
 document.addEventListener("click", function(e) {
   if (e.target.classList.contains("edit-me")) {
-    let userInput = prompt("Enter your desired new text")
-    axios.post('update-item', {text: userInput}).then(function() {
-      // do something here later
-    }).catch(function() {
-      console.log("Please try again later.")
-    })
+    let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+    if(userInput) {
+      axios.post('update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
+        e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+      }).catch(function() {
+        console.log("Please try again later.")
+      })
+    }
   }
 })
