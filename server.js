a651ccf2106c762f7f17cb6f1c1320283929c88c
brandonTsai <brandonya125@gmail.com>
2019-09-11 16:30:08 -0700

    Delete item both in client side and mongoDB

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', function(req, res) {
                   <span class="item-text">${item.text}</span>
                   <div>
                     <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                    <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
                   </div>
                 </li>`
             }).join('')}
@@ -69,6 +69,12 @@ app.post('/create-item', function(req, res) {
 
 app.post('/update-item', function(req, res) {
   db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+    res.send("Success")
+  })
+})
+
+app.post('/delete-item', function(req, res) {
+  db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, function() {
     res.send("Success")
   })
 })
