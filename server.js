4354c47ae0567fd0e1fa119e0ea0bd30962b46d8
brandonTsai <brandonya125@gmail.com>
2019-09-11 20:12:08 -0700

    Security concern

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let mongodb = require('mongodb')
+let sanitizeHTML = require('sanitize-html')
 
 let app = express()
 let db
@@ -14,6 +15,17 @@ mongodb.connect(connectString, {useNewUrlParser: true, useUnifiedTopology: true}
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+function passwordProtected(req, res, next) {
+  res.set('WWW-Authenticate', 'Basic realm="Simple Todo App"')
+  if (req.headers.authorization === "Basic bGVhcm46amF2YXNjcmlwdA==") {
+    next()
+  } else {
+    res.status(401).send("Authentication required")
+  }
+}
+
+app.use(passwordProtected)
 
 app.get('/', function(req, res) {
   db.collection('items').find().toArray(function(err, items) {
@@ -56,13 +68,15 @@ app.get('/', function(req, res) {
 })
 
 app.post('/create-item', function(req, res) {
-  db.collection('items').insertOne({text: req.body.text}, function(err, info) {
+  let safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {}})
+  db.collection('items').insertOne({text: safeText}, function(err, info) {
     res.json(info.ops[0])
   })
 })
 
 app.post('/update-item', function(req, res) {
-  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+  let safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {}})
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: safeText}}, function() {
     res.send("Success")
   })
 })
