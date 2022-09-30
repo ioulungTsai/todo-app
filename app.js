304040fd7268b7a0047e91d83727d957bcc14829
brandonTsai <brandonya125@gmail.com>
2022-09-30 15:22:17 -0700

    Create header.ejs & footer.ejs using EJS layout feature

--- app.js
+++ app.js
@@ -45,6 +45,10 @@ app.post("/work", (req, res) => {
   res.redirect("/work");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
