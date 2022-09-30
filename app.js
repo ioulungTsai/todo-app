6b825b736a3519c4af5a6282fe8e9bfb9750246f
brandonTsai <brandonya125@gmail.com>
2022-09-30 11:30:28 -0700

    Create add new item feature

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
-const express = require('express')
-const bodyparser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
+let items = [];
 
-app.get('/', (req, res) => {
-  let today = new Date()
-  let currentDay = today.getDay()
-  let day = ''
+app.set("view engine", "ejs");
 
-  switch(currentDay) {
-    case 0:
-      day = 'Sunday'
-      break
-    case 1:
-      day = 'Monday'
-      break
-    case 2:
-      day = 'Tuesday'
-      break
-    case 3:
-      day = 'Wednesday'
-      break
-    case 4:
-      day = 'Thursday'
-      break
-    case 5:
-      day = 'Friday'
-      break
-    case 6:
-      day = 'Saturday'
-      break
-  }
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  res.render('list', {kindOfDay: day})
+app.get("/", (req, res) => {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-})
+  let day = today.toLocaleDateString("en-US", options);
 
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
+});
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000')
-})
+  console.log("Server started on port 3000");
+});
